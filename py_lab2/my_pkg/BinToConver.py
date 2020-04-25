95cba0983486afa1c08a30abf0bd423d85825399
Seungsin <62578738+Seungsin@users.noreply.github.com>
2020-04-25 19:14:17 +0900

    Update BinToConver.py

--- py_lab2/my_pkg/BinToConver.py
+++ py_lab2/my_pkg/BinToConver.py
@@ -5,6 +5,8 @@ def conBin(bin):
 	size=len(bin)-1
 	for i in range(0,size):
 		if(bin[i]=='1'): num+= 2**(size-i)
+	if(size==0):
+		if(bin[size]=='1'): num = 1
 	return(num)
 def OCT(num):
 	print("=>OCT> %0o" %(num))
